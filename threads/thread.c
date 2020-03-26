0379009204f7c75475cb91a222959b0b95cf7b73
Daniel <dfblancos@kaist.ac.kr>
2020-03-26 18:27:52 +0900

    priority 2.2

--- threads/thread.c
+++ threads/thread.c
@@ -222,6 +222,9 @@ thread_create (const char *name, int priority,
 
 	/* Add to run queue. */
 	thread_unblock (t);
+	/* Yield if new thread has higher priority. */
+  if (priority > thread_current ()->priority)
+    thread_yield ();
 
 	return tid;
 }
@@ -260,8 +263,10 @@ thread_unblock (struct thread *t) {
 	list_push_back (&ready_list, &t->elem);
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
+	/*
 	if (t->priority > thread_current ()->priority)
 		thread_yield ();
+	*/
 }
 
 /* Adds current thread to the sleep_list and blocks it.
